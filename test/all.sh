1a8173ff8dc4f9ab6ae37a0dac1d42a167325a16
firstheart09 <1274455304@qq.com>
2024-08-26 10:09:19 +0800

    lastest updata

--- test/all.sh
+++ test/all.sh
@@ -1,5 +1,5 @@
 #/bin/bash
 ./build_bolt.sh
-perf record -e cycles:u -j any,u -o ../perf.data ../reorder
+# perf record -e cycles:u -j any,u -o ../perf.data ../reorder
 ./build_perf2bolt.sh &> branch.log
-
+# ./build_perf2bolt.sh
--- test/build_perf2bolt.sh
+++ test/build_perf2bolt.sh
@@ -1,6 +1,6 @@
 # 切换到测试目录
 cd $HOME/study/bolt/test
 # 使用perf2bolt生成文件
-perf2bolt -p perf.data -o perf.fdata ./reorder > branch_events.log
+perf2bolt -p /home/dushuai/study/Perf2Bolt/perf.data -o /home/dushuai/study/Perf2Bolt/perf.fdata /home/dushuai/study/Perf2Bolt/reorder
 # 查看log文件
-vim branch_events.log
+# vim branch_events.log
