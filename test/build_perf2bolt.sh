5d78c7d6ecf9690c6a38ca65e7f7a56c92c91161
firstheart09 <1274455304@qq.com>
2024-08-26 11:18:43 +0800

    Update test/build_perf2bolt.sh

--- test/build_perf2bolt.sh
+++ test/build_perf2bolt.sh
@@ -1,5 +1,5 @@
 # 切换到测试目录
-cd $HOME/study/bolt/test
+cd $HOME/study/Perf2Bolt/test
 # 使用perf2bolt生成文件
 perf2bolt -p /home/dushuai/study/Perf2Bolt/perf.data -o /home/dushuai/study/Perf2Bolt/perf.fdata /home/dushuai/study/Perf2Bolt/reorder
 # 查看log文件
